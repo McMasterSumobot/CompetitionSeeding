81202062a9ad68d33a14d32be1c55a899a98f18d
bcorbold <bradencorbold@gmail.com>
2018-03-28 20:48:52 -0400

    core: create all potential matches for each group

--- groupSeeding.js
+++ groupSeeding.js
@@ -1,15 +1,7 @@
-// based on number of teams, find best number of groups
-// randomly put teams from list into different groups
-// based on the smallest group size, determine the number of matches each team will play
-// create game match ups between teams in each group
-
 fs = require('fs');
+
 const pathToTeamList = './teamList.txt'; // each teams should be on it's own line, no empty lines!
 const outputFile = './Groups.csv';
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 
 function getTeamListFromTextFile(pathToTeamList) {
   return new Promise((fulfill, reject) => {
@@ -23,81 +15,80 @@ function getTeamListFromTextFile(pathToTeamList) {
   });
 }
 
-function removeExtraGroups(matchedGroups, groupLookup, minTeamsInGroup) {
-  for (let i = 0; i < matchedGroups[group.id].teams.length; i++) {
-    const team = matchedGroups[group.id].teams[i];
+function createAllMatchUps(groups) {
+  for (let i = 0; i < groups.length; i++) {
+    let group = groups[i];
+    let teams = group.teams;
+    let matchId = 0;
 
-    if(team.teamsFaced.length >= minTeamsInGroup) {
-      const indexA = getRandomInt(team.teamsFaced.length);
-      const teamToRemove = team.teamsFaced[indexA];
-      const indexB = matchedGroups[group.id].teams[groupLookup[teamToRemove]].teamsFaced.indexOf(team.name);
+    teams.forEach(teamA => {
+      teams.forEach(teamB => {
+        if (teamA !== teamB) {
 
-      if (matchedGroups[group.id].teams[groupLookup[teamToRemove]].teamsFaced.length >= minTeamsInGroup) {
-        matchedGroups[group.id].teams[i].teamsFaced.splice(indexA, 1);
-        matchedGroups[group.id].teams[groupLookup[teamToRemove]].teamsFaced.splice(indexB, 1);
+          if (group.matches.length === 0) {
+            group.matches.push({id: matchId, teams: [teamA, teamB]});
+            group[teamA]++;
+            group[teamB]++;
+          } else {
+            let matchAlreadyExists = false;
+            group.matches.forEach(match => {
+              if (teamA === match.teams[0] && teamB === match.teams[1]) {
+                matchAlreadyExists = true;
+              }
+              if (teamA === match.teams[1] && teamB === match.teams[0]) {
+                matchAlreadyExists = true;
+              }
+            });
+
+            if (!matchAlreadyExists) {
+              group.matches.push({id: matchId, teams: [teamA, teamB]});
+              group[teamA]++;
+              group[teamB]++;
+            }
+          }
+        }
+      });
+    });
+  }
+  return groups;
+}
+
+function removeExtraMatches(groups, matchesPerGroup) {
+  for (let i = 0; i < groups.length; i++) {
+    const group = groups[i];
+    const teamNames = group.teams;
+    let correctNumberOfMatches = true;
+    teamNames.forEach(name => {
+      if (group[name] > matchesPerGroup) {
+        correctNumberOfMatches = false;
       }
+    });
 
+    if (!correctNumberOfMatches) {
+      console.log('Too many matches!');
     }
   }
-  return matchedGroups;
+
+  return groups;
 }
 
-function createMatchUps(groups, minTeamsInGroup) {
-  const matchedGroups = [];
-
-  groups.forEach(group => {
-    matchedGroups.push({
-      id: group.id,
-      teams: []
-    });
-
-    let groupLookup = {};
-
-    group.teams.forEach((teamName, i) => {
-      const team = {
-        name: teamName,
-        teamsFaced: group.teams.filter(name => name !== teamName)
-      };
-      groupLookup[teamName] = i;
-      matchedGroups[group.id].teams.push(team);
-    });
-
-    while (tooManyMatches(matchedGroups)) {
-
-    }
-
-    matchedGroups = removeExtraGroups(matchedGroups, groupLookup, minTeamsInGroup);
-
-  });
-
-  console.log(JSON.stringify(matchedGroups, null, 2));
-}
-
-// this list is of team objects
-function createGroups(teamList) {
-  let numberOfGroups;
-
-  numberOfGroups = 4;
-  // if (teamList.length < 20) {
-  //   // 1-4 groups with min group size === 5
-  //   numberOfGroups = Math.floor(teamList.length / 5);
-  // } else if (teamList.length <= 1000) {
-  //   // 4 groups spread em out
-  //   numberOfGroups = 4;
-  // }
-
-  const minTeamsInGroup = Math.floor(teamList.length / numberOfGroups);
-  let currentGroup = 0;
-  const groups = [];
+getTeamListFromTextFile(pathToTeamList).then(teamList => {
+  const numberOfGroups = 4;
+  const matchesPerGroup = Math.floor(teamList.length / numberOfGroups) - 1;
+  
+  let groups = [];
 
   for(let i = 0; i < numberOfGroups; i++) {
     groups.push({
       id: i,
+      matches: [],
       teams: []
     });
   }
 
+  let currentGroup = 0;
   teamList.forEach(teamName => {
+    groups[currentGroup][teamName] = 0;
     groups[currentGroup].teams.push(teamName);
     currentGroup++;
     if (currentGroup >= numberOfGroups) {
@@ -105,12 +96,8 @@ function createGroups(teamList) {
     }
   });
 
-  createMatchUps(groups, minTeamsInGroup);
-}
+  groups = createAllMatchUps(groups);
+  groups = removeExtraMatches(groups, matchesPerGroup);
 
-
-
-
-getTeamListFromTextFile(pathToTeamList).then(teamList => {
-  createGroups(teamList);
+  // console.log(JSON.stringify(groups, null, 2));
 });
